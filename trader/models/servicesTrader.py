1b0402e6480e448321e773bbbd08129e6858e8b2
Sansan <shunngerold@gmail.com>
2025-07-23 07:02:12 +0800

    Added Services Data in DB

--- trader/models/servicesTrader.py
+++ trader/models/servicesTrader.py
@@ -7,9 +7,9 @@ class Services(models.Model):
     locationName = models.CharField(max_length=100, blank=True, null=True)
     marketName = models.CharField(max_length=100, blank=True, null=True)
     secondaryMarketName = models.CharField(max_length=100, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     is_active = models.BooleanField(default=True, help_text="Is this service currently active?")
 
     def __str__(self):
-        return self.keyword
+        return self.description
